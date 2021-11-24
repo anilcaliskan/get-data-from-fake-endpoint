4aaf155d28d81b6169be0c9888ab7ecbd74753d4
Cali <calisblood@yandex.com>
2021-11-24 20:08:14 +0300

    Fixed a few typos that were noticed later

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const getData = async(id) => {
-    const {data: users} = await axios(`https://jsonplaceholder.typicode.com/users/${id}`)
+    const {data: user} = await axios(`https://jsonplaceholder.typicode.com/users/${id}`)
     const {data: posts} = await axios(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
 
-    return {data: users, posts: posts}
+    return {user: user, posts: posts}
 }
 
 export default getData
